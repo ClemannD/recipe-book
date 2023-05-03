e0f9f719021c6c76620ecb2daa499c6d72aedaa3
Dylan <dmarco96@gmail.com>
2023-05-03 09:29:32 -0400

    fix reopen recipe bug

--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -223,6 +223,7 @@ const RecipesPage = () => {
                     }
 
                     setSelectedRecipe(recipe);
+                    setIsExpanded(true);
                   }}
                 />
               ))}
