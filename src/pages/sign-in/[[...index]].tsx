246652cc44d3f1b3b50acda61715802496ef9613
Dylan <dmarco96@gmail.com>
2023-05-07 22:50:09 -0400

    fix sign in link

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs';
 const SignInPage = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
-      <SignIn path="/sign-up" routing="path" signUpUrl="/sign-up" />
+      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-in" />
     </div>
   );
 };
