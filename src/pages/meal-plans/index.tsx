7b88ed5314d3efab5445da7b6522f551c8311f1a
Dylan <dmarco96@gmail.com>
2023-05-09 21:18:00 -0400

    meal plans page coming soon

--- src/pages/meal-plans/index.tsx
+++ src/pages/meal-plans/index.tsx
@@ -3,10 +3,19 @@ import Layout from '../../components/layout';
 const MealPlansPage = () => {
   return (
     <Layout>
-      <div className="p-10">
+      <div className="p-4 pt-20 lg:p-10 lg:pt-10">
         <div>
           <h1 className="text-2xl font-bold">Meal Plans</h1>
           <h2 className="mt-1 text-gray-600">Plan your meals for the week</h2>
+        </div>
+
+        <div className="flex w-full items-center justify-center gap-4 py-8">
+          <div className="rounded bg-white p-6 text-center shadow">
+            <h1 className="mb-2 text-2xl font-bold">
+              This page is a work in progress
+            </h1>
+            <h2 className="text-gray-600">Check back soon</h2>
+          </div>
         </div>
       </div>
     </Layout>
